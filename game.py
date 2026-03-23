a320d93802c634fde54ce613ac737b0d6d29cf6c
James <gabehoenstine@gmail.com>
2026-03-22 23:19:09 -0600

    Client version

---
+++ game.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 22 20:17:47 2026
+
+@author: jameshoenstine
+"""
+
+from gamefunctions import(
+    print_welcome,
+    print_shop_menu,
+    purchase_item,
+    random_monster
+    )
+
+
+def main():
+
+    name = input("Enter your name:")
+    print(print_welcome(name, 40))
+    print()
+    
+    print("Greetings!")
+    print("This is the magic shop")
+    print_shop_menu("frog",6, "wand", 40)
+    print()
+    
+    print("What do you want to purchase:")
+    item = float(input(("Enter Item price: ")))
+    money = float(input(("Enter starting money: ")))
+    quantity = int(input(("Enter quantity you want: ")))
+   
+    purchase_item (item, money, quantity)
+    
+    print()
+    
+    print("Watch out !")
+    monster = random_monster()
+
+
+if __name__ == "__main__":
+    main()
