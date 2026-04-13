5843d8c080a27d3b167437bc2a03cbfdfce591ee
James <gabehoenstine@gmail.com>
2026-04-13 17:53:24 -0600

    json

--- game.py
+++ game.py
@@ -12,23 +12,60 @@ from gamefunctions import (
     enter_bazaar,
     items,
     inventory,
-    equip_item
+    equip_item,
+    load_game,
+    save_game
 )
 
 
 
 
+
+
 def main():
-
-    name = input("Enter your name:")
-    print(f"Greetings {name}! You are in Sneep kingdom. ")
+    global data, p
     
-    hp = 50
-    power = random.randint(1,3)
-    gold = 50    
-
+    print("---- Sneep Kingdom Loader ----")
     
-    print(f"Current HP:{hp}, Current Gold:{gold}, Current Power:{power}")
+    file_load = input("Enter your file name or type 'new':")
+    
+    if file_load =="new":
+        data= None
+    else:
+        if not file_load.endswith(".json"):
+            file_load += ".json"
+    
+
+        data = load_game(file_load)
+    
+    if data:
+        p = data['player']
+        name = p.get('name')
+        
+        print(f"Welcome back {name}!")
+    else:
+        print("Starting a new game")
+        
+    
+        name = input("Enter your name:")
+        file_load = name.lower() + "json"
+        
+        p = {
+            "name": name,
+            "HP": 20,
+            "Power": 2,
+            "Gold":25,
+            "inventory":[]
+            
+            
+            }
+        print(f"Greetings {name}! You are in Sneep kingdom. ")
+    
+   
+    
+  
+    
+    print(f"Current HP:{p['HP']}, Current Gold:{p['Gold']}, Current Power:{p['Power']}")
     
     print("The Kingdom hums with activity. Where shall your curiosity lead you? ")
 
@@ -40,28 +77,28 @@ def main():
             " \nRest at the Snapping Turtle Inn(rest)."
            " \nAquire wares at the bazaar(shop)."
            "\nCheck inventory(check) and equip items."
-            "\nAbandon this adventure(quit).\n"
+            "\nSave and Abandon this adventure(quit).\n"
             "Input:"
             ).lower().strip()
         
         if path == "quest":
-            print(f"Your HP is: {hp} and power: {power}")
+            print(f"Your HP is: {p['HP']} and power: {p['Power']}")
             monster = random_monster()
-            while hp > 0 and monster['health'] > 0:
+            while p['HP'] > 0 and monster['health'] > 0:
                 a_f = input(f"You encountered a monster, do you attack (attack) or flee (flee)").lower().strip()
                 
                 if a_f == "attack":
-                  monster['health'] -= power
-                  hp -= monster['power']
+                  monster['health'] -= p['Power']
+                  p['HP'] -= monster['power']
                   
                   for item in inventory:
                       if item['name'] == "phantasmaclasm":
                           print("Phantasmaclasm is a one use spell, it is now gone")
                           inventory.remove(item)
-                          power = 2
+                          p['Power'] = 2
                           break
                               
-                  print(f"Monster health {monster['health']}, and your health {hp}")
+                  print(f"Monster health {monster['health']}, and your health {p['HP']}")
                 elif a_f == "flee":
                   print("You have fleed!")
                   break
@@ -69,12 +106,12 @@ def main():
                 else:
                     print("Invalid input... try again")
                 
-            if hp <= 0:
+            if p['HP'] <= 0:
                 print("You have fallen to the monster")
                 break
             elif monster['health'] <= 0:
                 print("You have beat the monster!")
-                gold += monster['money']
+                p['Gold'] += monster['money']
                 
                   
               
@@ -83,21 +120,21 @@ def main():
         
         elif path == "rest":
             
-            gold, hp = snapping_turtle_inn(gold, hp)
+             p['Gold'], p['HP'] = snapping_turtle_inn(p['Gold'], {p['HP']})
         
         elif path == "shop":
-            enter_bazaar(items,gold)
+           p['Gold'] = enter_bazaar(items,p['Gold'])
         
         elif path == "check":
             checking = True
             while checking:
                 print(f" Your inventory contians: {inventory}")
-                print(f" Your current power:{power}")
+                print(f" Your current power:{p['Power']}")
                 choice = input("\nWould you like to equip an item? (equip) or go back (back)").lower().strip()
                 
                 if choice == "equip":
                     
-                    power = equip_item(items,power)
+                    power = equip_item(items,p['Power'])
                 elif choice == "back":
                     checking = False
                 
@@ -107,20 +144,19 @@ def main():
         elif path == "quit":
             print(f"Until next time, {name}!")
             print("The gates of Sneep Kingdom close behind you.")
+            save_game(file_load, p)
             sys.exit()
         else:
             print("Invalid input...try again")
             
-
-    
-    
+ 
     
     
     
     
     
 
-main()
+
   
 
 
